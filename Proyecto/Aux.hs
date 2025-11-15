085c9118a6b5daab8df5029ad60e007d10c6f55e
Jafet <jafet@debianJFF.lap.com>
2025-11-14 18:14:03 -0600

    feat: acomodé las funciones auxiliares en el Aux.hs y dejé limpio el Proyecto.hs

--- Proyecto/Aux.hs
+++ Proyecto/Aux.hs
@@ -1,14 +1,20 @@
 module Aux where
+
+data AB = Vacio | Hoja Char | Nodo AB AB deriving (Eq, Show)
+
 {-
 funciones auxiliares de la primera
 -}
 
+-- Funcion que cuen ta el numero de apariciones de un Char
+
 cuentaA :: Char -> String -> Int
-cuentaA _ "" = 0
+cuentaA _ "" = 1
 cuentaA s (x:xs)
   | s == x = 1 + cuentaA s xs
   | otherwise = cuentaA s xs
 
+-- Funcion que elimina lo que ya se ha contado
 
 eliminaRepetidos :: Char -> String -> String
 eliminaRepetidos _ [] = []
@@ -16,11 +22,12 @@ eliminaRepetidos z (x:xs)
   | z == x = eliminaRepetidos z [y | y <- xs, y /= z]
   | otherwise = x : eliminaRepetidos z [y | y <- xs, y /= z]
 
+
 {-
 Funciones auxilares de la segunda
 -}
 
--- Funcion quickSort
+-- Funcion de ordenamiento
 
 quickSort :: (Ord b) => [(a, b)] -> [(a, b)]
 quickSort [] = []
@@ -28,4 +35,16 @@ quickSort ((a, b):xs) =
   let may = quickSort [x | x <- xs, snd x >= b]
       men = quickSort [x | x <- xs, snd x < b]
   in may ++ [(a, b)] ++ men
+
+--Funcion que ordena la lista de la frecuencia de los caracteres de una palabras
+
+ordfrec :: [(Char, Int)] -> [(Char, Int)]
+ordfrec [] = []
+ordfrec ((c, n):xs) = quickSort ((c, n):xs)
+
+-- Funcion que pasa la lista ordenada a Arbol de Hoffman
+
+arbol :: [(Char, Int)] -> AB
+arbol [] = Vacio
+arbol ((c, i):xs) = Nodo (arbol xs) (Hoja c)
 
--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -1,7 +1,5 @@
 import Aux
 
-data AB = Vacio | Hoja Char | Nodo AB AB deriving (Eq, Show)
-
 {-
 1. Dada una cadena de texto, regresar la frecuencia de cada uno de
 sus caracteres.
@@ -11,7 +9,7 @@ sus caracteres.
 
 cadfrec :: String -> [(Char, Int)]
 cadfrec [] = []
-cadfrec (x:xs) = (x, cuentaA x xs + 1) : cadfrec (eliminaRepetidos x xs)
+cadfrec (x:xs) = (x, cuentaA x xs) : cadfrec (eliminaRepetidos x xs)
 
 {-
 2. Función que dependiendo una cadena de texto se regrese el árbol de
@@ -19,23 +17,13 @@ Hoffman correspondiente.
     
 -}
 
---Funcion que ordena la lista de la frecuencia de los caracteres de una palabras
-
-ordfrec :: [(Char, Int)] -> [(Char, Int)]
-ordfrec [] = []
-ordfrec ((c, n):xs) = quickSort ((c, n):xs)
-
-arbol :: [(Char, Int)] -> AB
-arbol [] = Vacio
-arbol ((c, i):xs) = Nodo (arbol xs) (Hoja c)
-
-stree :: String -> (Char, Int) 
-stree "" = error "f"
-stree (x:xs) = head (cadfrec (x:xs))
+stree :: String -> AB 
+stree [] = Vacio
+stree palabra = arbol (ordfrec (cadfrec palabra)) 
 
 
 {-
-3. Funcion que codifica y comprime una cadena de text
+3. Funcion que codifica y comprime una cadena de texto
 codint
 -}
- 
+
